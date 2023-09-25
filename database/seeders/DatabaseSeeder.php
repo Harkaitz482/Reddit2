a924c3dba415a0b673cf5f5d64a8fc6096368a31
Harkaitz482 <harkatrujillo@hotmail.com>
2023-09-25 18:39:31 +0100

    A5.Finalizado

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,22 +1,32 @@
 <?php
 
 namespace Database\Seeders;
-
+// importar comunnity link
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\CommunityLink;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+ 
 
 class DatabaseSeeder extends Seeder
 {
     /**
+     * 
      * Seed the application's database.
      */
+    
     public function run(): void
     {
         // \App\Models\User::factory(10)->create();
-
+        DB::delete('delete from community_links');
+        CommunityLink::factory()->count(50)->create(); 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
     }
+
+    
 }
--- resources/views/community/index.blade.php
+++ resources/views/community/index.blade.php
@@ -1,4 +1,9 @@
 @extends('layouts.app')
 @section('content')
-<h1>Community</h1>
+@foreach ($links as $link)
+<li>{{$link->title}}</li>
+@endforeach
+{{$links->links()}}
+<small>Contributed by: {{$link->creator->name}} {{$link->updated_at->diffForHumans()}}</small>
+
 @stop
